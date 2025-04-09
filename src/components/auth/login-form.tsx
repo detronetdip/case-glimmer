8a963e0a564614707a216ee21ff1f426d51b8853
Ayondip <ayondip.jana@scalereal.com>
2025-04-09 20:07:00 +0530

    update

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -25,7 +25,7 @@ function LoginForm() {
                         {errorMessage && <p className="error-message">{errorMessage}</p>}
                         <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                                 <input
                                     type="text"
                                     id="username"
@@ -47,7 +47,7 @@ function LoginForm() {
                                     placeholder="••••••••"
                                 />
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <button type="submit" className="w-full bg-teal-500 text-white py-2 rounded-md">Sign in</button>
                         </form>
                     </div>
                 </div>
